cea7ffae500c37743d9dad1a8c149cd124d21005
priyapprabhu <168054419+priyapprabhu@users.noreply.github.com>
2024-06-06 15:56:53 +0530

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,39 @@
+'''ceasar cipher: Create a python program that can encrypt and decrypt text using 
+the Caesar Cipher algorithm. Allow users to input a message and a shift value to 
+perform encryption and decryption. '''
+
+def caesar_cipher(text, shift):
+    result=""
+    for char in text:
+        if char.isalpha():
+            shifted=ord(char) + shift
+
+            if char.isupper():
+                if shifted > ord('Z'):
+                    shifted-=26
+                elif shifted < ord("A"):
+                    shifted += 26
+        
+            elif char.islower():
+                if shifted > ord("z"):
+                    shifted-=26
+                elif shifted < ord("a"):
+                    shifted += 26
+            result +=chr(shifted)
+
+        else:
+            result +=char
+    return result
+
+
+def caesar_decipher(text, shift):
+    return caesar_cipher(text, -shift)
+
+text= input("Enter a message:")
+shift= int(input("enter a shift value:"))
+
+encrypted_text= caesar_cipher(text, shift)
+print("Encrypted:", encrypted_text)
+
+decrypted_text=caesar_decipher(encrypted_text, shift)
+print("Decrypted:", decrypted_text)
